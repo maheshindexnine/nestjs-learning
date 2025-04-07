3a20ee9dbe330ef386ca130786061e9c13439d73
Sushrita Banerjee <mahesh.gaikwad@indexnine.com>
2025-04-07 16:03:20 +0530

    learned controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { AppService, HelloObject } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  // @Redirect('https://google.com')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  @HttpCode(200)
+  postHello(@Body() body: Body, @Query() qur: any): any {
+    return this.appService.postHello();
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  // @Get('/asas')
+  // findAll(@Res() res: Response) {
+  //   res.status(HttpStatus.OK).json([]);
+  // }
+
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+export interface HelloObject {
+  data: string;
+  success: boolean;
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  postHello(): HelloObject {
+    return { data: 'hellow world', success: true };
   }
 }
